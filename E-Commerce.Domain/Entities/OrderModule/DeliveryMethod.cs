bbccddc6d262d94506f2cab8bf6117b26b736c39
Am66097 <amin1701899@science.menofia.edu.eg>
2025-12-10 19:32:43 +0200

    Order Module - Entities

---
+++ E-Commerce.Domain/Entities/OrderModule/DeliveryMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.OrderModule
+{
+    public class DeliveryMethod : BaseEntity<int>
+    {
+
+        public string ShortName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public decimal Price { get; set; }
+        public string DeliveryTime { get; set; } = default!;
+
+    }
+}
---
+++ E-Commerce.Domain/Entities/OrderModule/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.OrderModule
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public OrderAddress Address { get; set; } = default!;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!; 
+        public int DeliveryMethodId { get; set; } // Foreign Key
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>(); // Navigation Property =  []
+        public decimal Subtotal { get; set; } // Total Price of items before delivery
+        public decimal GetTotal() => Subtotal + DeliveryMethod.Price; // Total Price including delivery --> Subtotal + DeliveryMethod.Price
+
+    }
+}
